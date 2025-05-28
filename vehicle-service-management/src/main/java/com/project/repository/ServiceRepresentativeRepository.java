cece96c633e86323f79ea26c81e531573f6bef7c
yashutoxie <yashutoxie@gmail.com>
2025-05-28 19:05:53 +0530

    Update

--- vehicle-service-management/src/main/java/com/project/repository/ServiceRepresentativeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.entity.ServiceRepresentative;
-
-public interface ServiceRepresentativeRepository extends JpaRepository<ServiceRepresentative, Long> {
-
-}
--- vehicle-service-management/src/main/java/com/project/repository/VehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.entity.Vehicle;
-
-public interface VehicleRepository  extends JpaRepository<Vehicle, Long> {
-
-}
