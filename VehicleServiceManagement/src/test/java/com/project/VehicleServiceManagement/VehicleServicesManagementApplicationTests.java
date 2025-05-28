07b7c4c0cc9445d0dccec611bf92718e9449a8e6
yashutoxie <yashutoxie@gmail.com>
2025-05-28 19:25:14 +0530

    Add Project Files

--- VehicleServiceManagement/src/test/java/com/project/VehicleServiceManagement/VehicleServicesManagementApplicationTests.java
+++ VehicleServiceManagement/src/test/java/com/project/VehicleServiceManagement/VehicleServicesManagementApplicationTests.java
@@ -1,4 +1,4 @@
-package com.aits.VehicleServicesManagement;
+package com.project.VehicleServiceManagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
